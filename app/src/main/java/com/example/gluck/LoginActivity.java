7fb25f0924927d1375a3ba09641c02d5b99e7c11
Nicole2512 <53039255+Nicole2512@users.noreply.github.com>
2019-11-05 12:15:06 +0300

    more location 2

--- app/src/main/java/com/example/gluck/LoginActivity.java
+++ app/src/main/java/com/example/gluck/LoginActivity.java
@@ -150,28 +150,23 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
-    // Here, thisActivity is the current activity
+
     public boolean checkLocationPermission(){
 
     if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
 
-        // Permission is not granted
-        // Should we show an explanation?
+
         if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)) {
-            // Show an explanation to the user *asynchronously* -- don't block
-            // this thread waiting for the user's response! After the user
-            // sees the explanation, try again to request the permission.
+
         } else {
-            // No explanation needed; request the permission
+
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSIONS_REQUEST_LOCATION);
 
-            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-            // app-defined int constant. The callback method gets the
-            // result of the request.
+
         }
         return false;
     }else
@@ -185,20 +180,17 @@ public class LoginActivity extends AppCompatActivity {
                                            String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
+
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+
                 } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+
                 }
                 return;
             }
 
-            // other 'case' lines to check for other
-            // permissions this app might request.
+
         }
     }
 }
