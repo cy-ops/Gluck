ac20f812722d20f3976802e5e2d9878ee8359706
Nicole2512 <53039255+Nicole2512@users.noreply.github.com>
2019-11-15 19:00:04 +0300

    minor change

--- app/src/main/java/com/example/gluck/BarAdaptor.java
+++ app/src/main/java/com/example/gluck/BarAdaptor.java
@@ -1,17 +1,33 @@
 package com.example.gluck;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
 
 public class BarAdaptor extends FirestoreRecyclerAdapter<Bar, BarAdaptor.BarHolder> {
 
@@ -26,12 +42,14 @@ public class BarAdaptor extends FirestoreRecyclerAdapter<Bar, BarAdaptor.BarHold
         private TextView textViewLocation;
         private TextView textViewBrand;
         private TextView textViewOffer;
+        private ImageView placeitemimage;
 
         public BarHolder(@NonNull View itemView) {
             super(itemView);
             textViewBrand = itemView.findViewById(R.id.textViewBrand);
             textViewLocation = itemView.findViewById(R.id.textViewLocation);
             textViewOffer = itemView.findViewById(R.id.textViewOffer);
+            placeitemimage = itemView.findViewById(R.id.placeitemimage);
 
             itemView.setOnClickListener(new View.OnClickListener() {
 
@@ -47,10 +65,26 @@ public class BarAdaptor extends FirestoreRecyclerAdapter<Bar, BarAdaptor.BarHold
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull BarAdaptor.BarHolder barHolder, int i, @NonNull Bar bar) {
+    protected void onBindViewHolder(@NonNull final BarAdaptor.BarHolder barHolder, int i, @NonNull final Bar bar) {
         barHolder.textViewOffer.setText(bar.getOffer());
         barHolder.textViewLocation.setText(bar.getLocation());
         barHolder.textViewBrand.setText(bar.getBrand());
+
+
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReference(bar.getImage());
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).fit().into(barHolder.placeitemimage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("ImgNotFound", e.toString());
+            }
+        });
+
+
     }
 
     @NonNull
@@ -58,6 +92,8 @@ public class BarAdaptor extends FirestoreRecyclerAdapter<Bar, BarAdaptor.BarHold
     public BarAdaptor.BarHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.place_item,
                 parent, false);
+
+
         return new BarHolder(v);
     }
 
--- app/src/main/java/com/example/gluck/InputActivity.java
+++ app/src/main/java/com/example/gluck/InputActivity.java
@@ -3,12 +3,41 @@ package com.example.gluck;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class InputActivity extends AppCompatActivity {
+
+    private EditText editTextBrand;
+    private EditText editTextOffer;
+    private EditText editTextAbout;
+    private EditText editLocation;
+    private Spinner category_spinner;
+    private Button submit_content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+
+
+        editTextBrand = findViewById(R.id.editTextBrand);
+        editTextOffer = findViewById(R.id.editTextOffer);
+        editTextAbout = findViewById(R.id.editTextAbout);
+        editLocation = findViewById(R.id.editLocation);
+        category_spinner = findViewById(R.id.category_spinner);
+        submit_content = findViewById(R.id.submit_content);
+
+        String[] category = new String[]{"Restaurant","Bar"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.spinner_text, category);
+
+        adapter.setDropDownViewResource(R.layout.spinner_text);
+
+        category_spinner.setAdapter(adapter);
+
+
     }
 }
